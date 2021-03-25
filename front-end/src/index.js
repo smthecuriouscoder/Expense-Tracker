dc9fc66ef6316c2e5b2ce02700ee56570adc1e5b
Sagar Mittal <mittalsaggar555@gmail.com>
2021-03-25 19:20:27 +0530

    Some Updations

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { SpeechProvider } from '@speechly/react-client';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { SpeechProvider } from "@speechly/react-client";
 
 ReactDOM.render(
-  <SpeechProvider appId="01491183-a7f5-4316-a404-6aefbd641ae9"  language="en-US" >
+  <SpeechProvider appId={process.env.REACT_APP_SPEECHLY_APPID} language='en-US'>
     <App />
   </SpeechProvider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
